37ff293f490fa39413d40dd49e958e08e118425e
LarisaGl <89888985265@mail.ru>
2018-05-14 00:30:23 +0300

    corrections

--- array.php
+++ array.php
@@ -10,51 +10,50 @@ $continents= array(
 	"Antarctica" => array ("Ursus maritimus", "Spheniscidae"),
 	);
 
+	$word_count=[];
+	$animal=[];
+	$full_array=[];
+
 	foreach ($continents as $key => $value) {
-		$cont=$key;
 		foreach ($value as $animal) {
-			$a=str_word_count($animal, 1);
-			$b=count($a);
-			/*echo "<pre>";
-			print_r($a);
-			echo "</pre>";
-			echo "<pre>";
-			print_r($b);
-			echo "</pre>";*/
-			if ($b==2) {
-				foreach ($a as $v) {
-					$new_array[]=$v;					
+			$word_count=str_word_count($animal, 1);
+			$count=count($word_count);
+			if ($count==2) {
+				foreach ($word_count as $v) {
+					$full_array[]=$v;					
 				}
 			}
 		}
 	}
 
-	/*echo "<pre>";
-	print_r($new_array);
-	echo "</pre>";*/
+	$odd=[];
+	$even=[];
 
-	foreach ($new_array as $k => $v) {
-		if ($k % 2 !=0)
-			$arr2[]=$v;
-		else $arr1[]=$v;
+	foreach ($full_array as $key => $value) {
+		if ($key % 2 !=0)
+			$odd[]=$value;
+		else $even[]=$value;
+	}
+
+	shuffle($odd);
+	shuffle($even);
+
+	echo "<pre>";
+	print_r($even);
+	echo "</pre>";	
+
+	echo "<pre>";
+	print_r($odd);
+	echo "</pre>";
+
+	$finish=[];
+
+	foreach ($even as $key => $value) {
+		$finish[$key]=$value." ".$odd[$key];
 	}
 
 	echo "<pre>";
-	print_r($arr1);
-	echo "</pre>";
-
-	shuffle($arr2);
-
-	echo "<pre>";
-	print_r($arr2);
-	echo "</pre>";
-
-	foreach ($arr1 as $k => $v) {
-		$res[$k]=$v." ".$arr2[$k];
-	}
-
-	echo "<pre>";
-	print_r($res);
+	print_r($finish);
 	echo "</pre>";
 
 ?>
