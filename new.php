590708cfb3b4e753e54f18ee51bee3effe76cb89
LarisaGl <89888985265@mail.ru>
2018-07-07 23:22:48 +0300

    full

--- new.php
+++ new.php
@@ -1,5 +1,22 @@
 <?php
-foreach ($continents as $key => $value) {
+
+$continents= array(
+	"Europe" => array ("Canis lupus", "Martes"),
+	"Asia" => array ("Ursus arctos", "Ailuropoda melanoleuca"),
+	"Africa" => array ("Panthera leo", "Rhinocerotidae"),
+	"Australia" => array ("Macropus", "Phascolarctos cinereus"),
+	"NorthAmerica" => array ("Canis latrans", "Hyaenidae"),
+	"South America" => array ("Cingulata", "Myrmecophagidae"),
+	"Antarctica" => array ("Ursus maritimus", "Spheniscidae"),
+	);
+
+	$count=[];
+	$full_array=[];
+	$first=[];
+	$second=[];
+	$exp=[];
+
+	foreach ($continents as $key => $value) {
 	$cont=$key;
 	foreach ($value as $animal) {
 		$pos = strpos($animal, ' ');
@@ -13,3 +30,35 @@ foreach ($continents as $key => $value) {
 			}
 	}
 }
+
+    echo "<pre>";
+	print_r($false_array);
+	echo "</pre>";
+
+	echo "<pre>";
+	print_r($full_array);
+	echo "</pre>";
+
+	shuffle($first);
+	shuffle($second);
+
+	echo "<pre>";
+	print_r($first);
+	echo "</pre>";	
+
+	echo "<pre>";
+	print_r($second);
+	echo "</pre>";
+
+	$finish=[];
+
+	foreach ($first as $key => $value) {
+		$finish[$key]=$value." ".$second[$key];
+	}
+
+	echo "<pre>";
+	print_r($finish);
+	echo "</pre>";
+
+	$imp=implode(",", $finish);
+	echo $imp;
